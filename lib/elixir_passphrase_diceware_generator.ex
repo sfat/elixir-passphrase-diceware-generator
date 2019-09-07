fa828e5a66c37fd318beecfba6ea357957c880d1
Andrei Sfat <andrei@sfat.me>
2019-09-07 21:42:21 +0300

    added skeleton project

---
+++ lib/elixir_passphrase_diceware_generator.ex
@@ -0,0 +1,18 @@
+defmodule ElixirPassphraseDicewareGenerator do
+  @moduledoc """
+  Documentation for ElixirPassphraseDicewareGenerator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirPassphraseDicewareGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_passphrase_diceware_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPassphraseDicewareGeneratorTest do
+  use ExUnit.Case
+  doctest ElixirPassphraseDicewareGenerator
+
+  test "greets the world" do
+    assert ElixirPassphraseDicewareGenerator.hello() == :world
+  end
+end
