40227065337768ca8c07d93d24f88466378aaa2b
Andrei Sfat <andrei@sfat.me>
2019-09-16 20:41:48 +0300

    Changed name of the constant creating the file stream

--- apps/library/lib/file_loader.ex
+++ apps/library/lib/file_loader.ex
@@ -3,13 +3,13 @@ defmodule Library.FileLoader do
   Module responsible for retrieval of words used by the diceware generator
   """
   @otp_app Mix.Project.config()[:app]
-  @bar_contents File.stream!(Application.app_dir(@otp_app, "priv/eff_large_wordlist.txt"))
+  @word_list_contents File.stream!(Application.app_dir(@otp_app, "priv/eff_large_wordlist.txt"))
 
   @doc """
   Load words from file
   """
   def load_words do
-    @bar_contents
+    @word_list_contents
     |> Stream.map(&String.trim(&1))
     |> Stream.map(&String.split(&1, "\t"))
     |> Stream.map(fn [a, b] -> %{number: String.to_integer(a), word: b} end)
